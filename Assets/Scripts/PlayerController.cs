a6cebb2dc10beea4d33084578d0d60a80b6f93f2
Jaimerson Araújo <jaimerson@toptal.com>
2018-10-10 18:28:15 -0300

    Create player animations and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+		transform.Translate(x, y, 0);
+	}
+}

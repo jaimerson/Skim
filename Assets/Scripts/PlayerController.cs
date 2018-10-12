269fbeb504a9391ec13a8440d3c5c0aa5b610ea3
James <jaimersonaraujo@gmail.com>
2018-10-12 12:03:11 -0300

    Fix player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,30 @@
-﻿using UnityEngine;
+using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	
+	Animator animator;
+	string direction;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
-		var y = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+		float x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+		float y = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+		animator.SetFloat("WalkingSpeed", Math.Max(Math.Abs(x), Math.Abs(y)));
+
+		if(x == 0 && y == 0){
+			return;
+		}
+
+		if(Math.Abs(x) > Math.Abs(y)){
+			direction = x > 0 ? "Right" : "Left";
+		}else{
+			direction = y > 0 ? "Up" : "Down";
+		}
+		animator.SetTrigger("Look" + direction);
 
 		transform.Translate(x, y, 0);
 	}

896a1673c22d865cd2be8128285400ff31438239
Jaimerson Araújo <jaimerson@toptal.com>
2018-10-18 15:57:16 -0300

    Add trigger for opening chest

--- Assets/Scripts/TreasureChestController.cs
+++ Assets/Scripts/TreasureChestController.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TreasureChestController : MonoBehaviour {
+	Animator animator;
+	bool open;
+	bool nearPlayer;
 
 	// Use this for initialization
 	void Start () {
-		
+		animator = GetComponent<Animator>();
+		open = false;
+		nearPlayer = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(nearPlayer){
+			if(Input.GetKey("e")){
+				animator.SetTrigger("open");
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collider){
+		Debug.Log("collided");
+		if(open){
+			return;
+		}else if(collider.gameObject.tag == "Player"){
+			nearPlayer = true;
+		}
 	}
 }

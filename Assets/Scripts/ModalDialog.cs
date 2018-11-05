1e3466902718e9dd7959090e9f1510f6f4a63a62
Jaimerson Araújo <jaimerson@toptal.com>
2018-11-05 18:03:51 -0300

    Make the modal dialog a prefab

---
+++ Assets/Scripts/ModalDialog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ModalDialog : MonoBehaviour {
+	public Text text;
+	public Text buttonText;
+	public Button acceptButton;
+
+	// Use this for initialization
+	void Start () {
+		acceptButton.onClick.RemoveAllListeners();
+		acceptButton.onClick.AddListener(Exit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("return")){
+			Exit();
+		}
+	}
+
+	void Exit(){
+		Destroy(gameObject);
+	}
+}

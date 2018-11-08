1c6477856301f3c115b0bace2c59e65777af25e0
Jaimerson Araújo <jaimerson@toptal.com>
2018-11-08 20:47:22 -0300

    Finalize the first puzzle o/

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour {
 
+    public string sceneName;
+
 	void OnTriggerEnter2D(Collider2D collider){
 		StartCoroutine(GameObject.Find("Main Camera").GetComponent<CameraController>().fadeToBlack(
-			() => SceneManager.LoadScene("Entrance")
+			() => SceneManager.LoadScene(sceneToLoad())
 		));
+	}
+
+	protected virtual string sceneToLoad(){
+		// 0 = left
+		// 1 = middle
+		// 2 = right
+		LabyrinthExit.CorrectIndexes = new int[]{0,1,0,2,0,1,2,1,0,0};
+		return sceneName;
 	}
 }

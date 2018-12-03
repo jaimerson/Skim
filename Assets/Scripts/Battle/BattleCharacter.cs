9a53cd66cf9e391464604e76299d89a90a26e353
James <jaimersonaraujo@gmail.com>
2018-12-03 16:07:47 -0300

    wip

--- Assets/Scripts/Battle/BattleCharacter.cs
+++ Assets/Scripts/Battle/BattleCharacter.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleCharacter : MonoBehaviour {
+public class BattleCharacter {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Character character;
+	public GameObject gameObject;
+	private Animator animator;
+
+	public BattleCharacter(Character character){
+		this.character = character;
+		this.gameObject = gameObjectFromCharacter(character);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private GameObject gameObjectFromCharacter(Character c){
+		UnityEngine.Object prefab = Resources.Load(string.Format("Prefabs/Battle/{0}",character.prefabPath));
+		return MonoBehaviour.Instantiate(prefab) as GameObject;
 	}
 }

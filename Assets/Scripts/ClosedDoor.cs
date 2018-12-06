25d1f6508d435de26d4cc566550228408a61e9ba
Shirley Ohara Telemaco de Freitas <shirleyohara@imd.local>
2018-12-05 22:21:42 -0300

    Creating the closed door script class

---
+++ Assets/Scripts/ClosedDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosedDoor : MonoBehaviour {
+    public bool enable;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Exit.GetFinishedPuzzle1();
+    }
+
+    public void Close () {
+        gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -8,9 +8,12 @@ public class Exit : MonoBehaviour {
     public string sceneName;
     public Vector3 position;
     public static Vector3 playerPosition;
+    private bool finishedPuzzle1 = false;
 
 	void OnTriggerEnter2D(Collider2D collider){
         SceneHelper.GoToScene(sceneToLoad());
+
+
         if (position != null) {
             Exit.playerPosition = position;
         }
@@ -25,8 +28,15 @@ public class Exit : MonoBehaviour {
 		// 1 = middle
 		// 2 = right
 		LabyrinthExit.CorrectIndexes = new int[]{0,1,0,2,0,1,2,1,0,0};
+
+        finishedPuzzle1 = true;
+
 		return sceneName;
 	}
+
+    public bool GetFinishedPuzzle1 () {
+        return finishedPuzzle1;
+    }
 }
 //x 36.98
 //y -0.25
